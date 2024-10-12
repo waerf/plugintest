8f6fdab6f402756cc2f3dabd0cf55dbdd3268dc6
GitHub Action <action@github.com>
2024-10-12 13:34:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/waerf/plugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/waerf/plugintest/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.waerf.plugintest.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.waerf.plugintest.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
